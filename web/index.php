d4574738e6364ddd6ae3aca92a882c32df60a205
nazha <n.oularabi@gmail.com>
2017-07-15 18:09:23 -0400

    initial code

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+$app->get('/galerie', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('galerie.twig');
+});
+
+$app->get('/nousJoindre', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('nousJoindre.twig');
+});
+$app->run();
